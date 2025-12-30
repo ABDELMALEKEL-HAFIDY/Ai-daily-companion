a197e07f46cd104dbb6c18f7c4bc9f73ad19928d
ABDELMALEKEL-HAFIDY <abdelmalekelhafidy@gmail.com>
2025-12-30 04:37:06 +0100

    create onbording

--- lib/features/on_boarding/presentation/view/on_boarding_view.dart
+++ lib/features/on_boarding/presentation/view/on_boarding_view.dart
@@ -1,10 +1,11 @@
+import 'package:ai_daily_companion/features/on_boarding/presentation/view/widgets/on_boarding_view_body.dart';
 import 'package:flutter/material.dart';
 
 class OnBoardingView extends StatelessWidget {
   const OnBoardingView({super.key});
-static const routeName = 'onBordingView';
+  static const routeName = 'onBordingView';
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(body: OnBoardingViewBody());
   }
 }
